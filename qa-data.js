525df6fef749fef0d73517422f8f642f6c672071
ri32ph <130022373+ri32ph@users.noreply.github.com>
2026-07-31 07:09:47 +0900

    Add files via upload

---
+++ qa-data.js
@@ -0,0 +1,490 @@
+const QA_DATA=[
+  {
+    "no": "Q",
+    "title": "HPVに感染してもほとんどは自然治癒するので、ワクチンを受けなくてもよいのではないでしょうか。",
+    "category": "子宮頸がんについて",
+    "keywords": "HPV ワクチン 接種対象者 小学生",
+    "url": "https://www.know-vpd.jp/faq/hpv/",
+    "sourceName": "Know VPD!"
+  },
+  {
+    "no": "Q",
+    "title": "HPVは、ありふれたウイルスと聞きました。わざわざワクチンで予防する必要はありますか。",
+    "category": "子宮頸がんについて",
+    "keywords": "HPV ワクチン",
+    "url": "https://www.know-vpd.jp/faq/hpv/",
+    "sourceName": "Know VPD!"
+  },
+  {
+    "no": "Q",
+    "title": "HPVワクチンのキャッチアップ接種は、今からでも間に合いますか。",
+    "category": "キャッチアップ接種",
+    "keywords": "HPV ワクチン キャッチアップ",
+    "url": "https://www.know-vpd.jp/faq/hpv/",
+    "sourceName": "Know VPD!"
+  },
+  {
+    "no": "Q",
+    "title": "HPVワクチンの定期接種（接種の積極的な勧奨）が再開になった理由を教えてください。",
+    "category": "積極的勧奨の差し控え",
+    "keywords": "HPV ワクチン 積極的な勧奨 定期接種",
+    "url": "https://www.know-vpd.jp/faq/hpv/",
+    "sourceName": "Know VPD!"
+  },
+  {
+    "no": "Q",
+    "title": "HPVワクチンは、医療機関に行けばいつでも接種できますか。",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 接種方法",
+    "url": "https://www.know-vpd.jp/faq/hpv/",
+    "sourceName": "Know VPD!"
+  },
+  {
+    "no": "Q",
+    "title": "HPVワクチンは、子宮頸がんそのものを予防する効果が証明されていないって本当ですか。",
+    "category": "検診・ワクチンについて",
+    "keywords": "子宮けいがん HPV ワクチン",
+    "url": "https://www.know-vpd.jp/faq/hpv/",
+    "sourceName": "Know VPD!"
+  },
+  {
+    "no": "Q",
+    "title": "HPVワクチンは、本当に子宮頸がんを減らすことができるのでしょうか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "子宮けいがん HPV ワクチン",
+    "url": "https://www.know-vpd.jp/faq/hpv/",
+    "sourceName": "Know VPD!"
+  },
+  {
+    "no": "Q",
+    "title": "HPVワクチンは男子も受けた方がいいのですか。",
+    "category": "男子・その他",
+    "keywords": "HPV ワクチン 男性 男",
+    "url": "https://www.know-vpd.jp/faq/hpv/",
+    "sourceName": "Know VPD!"
+  },
+  {
+    "no": "Q",
+    "title": "ワクチンを接種しても、子宮頸がんになる可能性は０ではないと聞きました。接種する意味はありますか。",
+    "category": "検診・ワクチンについて",
+    "keywords": "子宮けいがん HPV ワクチン",
+    "url": "https://www.know-vpd.jp/faq/hpv/",
+    "sourceName": "Know VPD!"
+  },
+  {
+    "no": "Q",
+    "title": "高校生です。今からでもHPVワクチンを受けられますか。",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 接種対象者 高校生",
+    "url": "https://www.know-vpd.jp/faq/hpv/",
+    "sourceName": "Know VPD!"
+  },
+  {
+    "no": "Q",
+    "title": "子宮頸がんの予防対策には子宮がん検診がありますが、なぜ、検診だけではいけないのですか。",
+    "category": "検診・ワクチンについて",
+    "keywords": "子宮けいがん HPV ワクチン がん検診",
+    "url": "https://www.know-vpd.jp/faq/hpv/",
+    "sourceName": "Know VPD!"
+  },
+  {
+    "no": "Q",
+    "title": "子宮頸がんは大人の病気ですよね。ワクチンを接種する時期が早すぎるのではないでしょうか。",
+    "category": "検診・ワクチンについて",
+    "keywords": "子宮けいがん HPV ワクチン 子供",
+    "url": "https://www.know-vpd.jp/faq/hpv/",
+    "sourceName": "Know VPD!"
+  },
+  {
+    "no": "Q",
+    "title": "小学校６年生で接種を進めているのは、なぜですか。",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 接種対象者 小学生",
+    "url": "https://www.know-vpd.jp/faq/hpv/",
+    "sourceName": "Know VPD!"
+  },
+  {
+    "no": "Q",
+    "title": "定期接種では、効果が高いHPVワクチンを受けられますか。",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 定期接種",
+    "url": "https://www.know-vpd.jp/faq/hpv/",
+    "sourceName": "Know VPD!"
+  },
+  {
+    "no": "問1-1",
+    "title": "子宮頸がんとは何ですか？",
+    "category": "子宮頸がんについて",
+    "keywords": "子宮けいがん 病気",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q1-1",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問1-2",
+    "title": "何が原因で子宮頸がんになるのですか？",
+    "category": "子宮頸がんについて",
+    "keywords": "原因 HPV 感染 性的接触",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q1-2",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問1-3",
+    "title": "子宮頸がんにかかるとどのような症状が現れますか？",
+    "category": "子宮頸がんについて",
+    "keywords": "子宮けいがん 病気 症状",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q1-3",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問1-4",
+    "title": "子宮頸がんは、どれくらい重い病気ですか？",
+    "category": "子宮頸がんについて",
+    "keywords": "子宮けいがん 病気 症状",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q1-4",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問1-5",
+    "title": "子宮頸がんの患者さんはどれくらいいるのですか？",
+    "category": "子宮頸がんについて",
+    "keywords": "子宮けいがん 病気 症状",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q1-5",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問1-6",
+    "title": "子宮頸がんで亡くなる方はどれくらいいるのですか？",
+    "category": "子宮頸がんについて",
+    "keywords": "子宮けいがん 病気 症状",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q1-6",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問1-7",
+    "title": "ヒトパピローマウイルスとは何ですか？",
+    "category": "子宮頸がんについて",
+    "keywords": "子宮けいがん HPV ウイルス 陰茎がん 肛門がん 中咽頭がん",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q1-7",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問1-8",
+    "title": "ヒトパピローマウイルスはどれくらい感染しやすいものですか？",
+    "category": "子宮頸がんについて",
+    "keywords": "子宮けいがん HPV ウイルス 感染",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q1-8",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問1-9",
+    "title": "ヒトパピローマウイルスに感染すると必ずがんになるのですか？",
+    "category": "子宮頸がんについて",
+    "keywords": "子宮けいがん HPV",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q1-9",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-1",
+    "title": "子宮頸がんを予防する方法はありますか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "子宮けいがん HPV ワクチン",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-1",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-10",
+    "title": "HPVワクチンはどれくらい効くのですか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 9価 効果",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-10",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-11",
+    "title": "海外でのHPVワクチンの使用状況を教えてください。",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 海外",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-11",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-12",
+    "title": "子宮頸がん検診とHPVワクチンは両方受けなければいけませんか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "子宮けいがん HPV ワクチン がん検診",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-12",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-13",
+    "title": "HPVワクチンについて、がんを予防する効果は証明されていないのですか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "子宮けいがん HPV ワクチン 予防",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-13",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-14",
+    "title": "HPVワクチンは痛いのですか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 副反応",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-14",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-15",
+    "title": "HPVワクチン接種後に副反応はありますか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 副反応",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-15",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-16",
+    "title": "HPVワクチン接種後に報告されている「多様な症状」はどのようなものですか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 副反応",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-16",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-17",
+    "title": "HPVワクチン接種後に気になる症状が出た場合、どこに相談すればよいですか？",
+    "category": "接種後の症状・医療体制",
+    "keywords": "HPV ワクチン 副反応 接種後 協力医療機関",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-17",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-18",
+    "title": "HPVワクチンを受ける際に注意することはありますか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-18",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-19",
+    "title": "HPVワクチン接種後に注意することはありますか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 接種後",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-19",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-2",
+    "title": "子宮頸がん検診はどのようなものですか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "子宮けいがん がん検診",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-2",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-20",
+    "title": "予防接種されるワクチンの安全性はどのようにチェックしていますか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 安全性",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-20",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-21",
+    "title": "予防接種を受けた後に体調が悪くなり、医療機関を受診しました。救済などはありますか？",
+    "category": "接種後の症状・医療体制",
+    "keywords": "HPV ワクチン 副反応 接種後 予防接種健康被害救済制度",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-21",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-22",
+    "title": "定期接種の対象者ですが、令和５年３月31日までに任意接種としてシルガード９を受けました。償還払いを受けられますか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 定期接種 費用",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-22",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-3",
+    "title": "HPVワクチンの接種はどのようなものですか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-3",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-4",
+    "title": "HPVワクチンの接種場所など、必要な情報はどこに問い合わせたらよいですか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-4",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-5",
+    "title": "HPVワクチンは絶対に受けなければならないものですか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-5",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-6",
+    "title": "HPVワクチンは何回接種すればよいですか、またその接種方法はどのようにすれば良いですか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 接種方法",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-6",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-7",
+    "title": "令和８年度から定期接種は９価HPVワクチンだけになりますが、２価・４価で開始し接種が完了していない場合はどうすれば良いですか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 9価",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-7",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-8",
+    "title": "HPVワクチンの定期接種の対象年齢を過ぎてからでも、接種できますか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 接種方法 定期接種 対象年齢",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-8",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問2-9",
+    "title": "HPVワクチンを１回または２回接種してから１年を過ぎてしまいました。次の接種はどうすればよいですか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 接種方法",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q2-9",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問3-1",
+    "title": "ワクチン接種後に生じた症状に対する診療体制はどうなっていますか？",
+    "category": "接種後の症状・医療体制",
+    "keywords": "HPV ワクチン 副反応 接種後 協力医療機関",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q3-1",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問4-1",
+    "title": "なぜ、平成９年度～平成19年度生まれの女性に、あらためて接種の機会が設けられていたのですか？",
+    "category": "キャッチアップ接種",
+    "keywords": "HPV ワクチン キャッチアップ",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q4-1",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問5-1",
+    "title": "予防接種対象者への積極的な勧奨が差し控えられていたのはなぜでしょうか？",
+    "category": "積極的勧奨の差し控え",
+    "keywords": "HPV ワクチン 積極的な勧奨",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q5-1",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問5-2",
+    "title": "積極的な勧奨が差し控えられている状態が終了したのはなぜでしょうか？",
+    "category": "積極的勧奨の差し控え",
+    "keywords": "HPV ワクチン 積極的な勧奨",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q5-2",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問5-3",
+    "title": "積極的な勧奨が差し控えられている状態が終了しましたが、具体的にどのようにかわったのでしょうか？",
+    "category": "積極的勧奨の差し控え",
+    "keywords": "HPV ワクチン 積極的な勧奨",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q5-3",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "問5-4",
+    "title": "接種対象者への周知はどのように行われているのですか？",
+    "category": "積極的勧奨の差し控え",
+    "keywords": "HPV ワクチン 接種対象者",
+    "url": "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/hpv_qa.html#Q5-4",
+    "sourceName": "厚生労働省"
+  },
+  {
+    "no": "Q6",
+    "title": "2価、4価HPVワクチン接種を受けた後に、9価HPVワクチンを追加することはできますか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 9価",
+    "url": "https://jsgo.or.jp/hpvqa/contents.html",
+    "sourceName": "日本婦人科腫瘍学会"
+  },
+  {
+    "no": "Q2",
+    "title": "HPVワクチンの定期接種は、なぜ小学校６年生から高校１年生（12〜16歳）までなのですか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 定期接種 小学生 中学生 高校生",
+    "url": "https://jsgo.or.jp/hpvqa/contents.html",
+    "sourceName": "日本婦人科腫瘍学会"
+  },
+  {
+    "no": "Q9",
+    "title": "HPVワクチンを接種するかどうか判断するのに、HPV検査をうける必要はありますか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン HPV検査",
+    "url": "https://jsgo.or.jp/hpvqa/contents.html",
+    "sourceName": "日本婦人科腫瘍学会"
+  },
+  {
+    "no": "Q3",
+    "title": "がんを予防する効果は証明されているのですか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン がん 予防",
+    "url": "https://jsgo.or.jp/hpvqa/contents.html",
+    "sourceName": "日本婦人科腫瘍学会"
+  },
+  {
+    "no": "Q5",
+    "title": "ワクチン接種による副反応としてどのようなことを考えておくと良いですか？ もし、そのような症状があった時にはどうしたら良いですか？",
+    "category": "接種後の症状・医療体制",
+    "keywords": "HPV ワクチン 副反応 接種後",
+    "url": "https://jsgo.or.jp/hpvqa/contents.html",
+    "sourceName": "日本婦人科腫瘍学会"
+  },
+  {
+    "no": "Q1",
+    "title": "現在、HPVワクチン接種を受けることはできるのですか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン",
+    "url": "https://jsgo.or.jp/hpvqa/contents.html",
+    "sourceName": "日本婦人科腫瘍学会"
+  },
+  {
+    "no": "Q8",
+    "title": "子宮頸がん検診で異常があった場合や、子宮頸部円錐切除術やレーザー蒸散術を受けた後で、HPVワクチン接種を受けるのは、その後の病気の再発を予防する効果がありますか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "子宮けいがん HPV ワクチン がん検診 異常 手術 再発",
+    "url": "https://jsgo.or.jp/hpvqa/contents.html",
+    "sourceName": "日本婦人科腫瘍学会"
+  },
+  {
+    "no": "Q10",
+    "title": "新型コロナウイルスワクチンの接種との関係はどうですか？",
+    "category": "男子・その他",
+    "keywords": "新型コロナウイルス HPV ワクチン",
+    "url": "https://jsgo.or.jp/hpvqa/contents.html",
+    "sourceName": "日本婦人科腫瘍学会"
+  },
+  {
+    "no": "Q7",
+    "title": "定期接種以外の年齢でワクチン接種を受けることはできますか？ その効果はどうですか？ また、何歳まで接種を受けることができますか？",
+    "category": "キャッチアップ接種",
+    "keywords": "HPV ワクチン 接種対象者 定期接種",
+    "url": "https://jsgo.or.jp/hpvqa/contents.html",
+    "sourceName": "日本婦人科腫瘍学会"
+  },
+  {
+    "no": "Q4",
+    "title": "日本国内では、HPVワクチンの有効性は証明されているのですか？",
+    "category": "検診・ワクチンについて",
+    "keywords": "HPV ワクチン 有効性",
+    "url": "https://jsgo.or.jp/hpvqa/contents.html",
+    "sourceName": "日本婦人科腫瘍学会"
+  }
+];
